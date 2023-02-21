437b2f08cc4e6291db0ab948a3851a305ca75fca
winglovecola@gmail.com <winglovecola@gmail.com>
2023-02-21 06:22:18 -0500

    completed the app structure and the basic functions

---
+++ db/schema.sql
@@ -0,0 +1,42 @@
+DROP DATABASE IF EXISTS emanager_db;
+CREATE DATABASE emanager_db;
+
+USE emanager_db;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  ON DELETE restrict
+);
+
+
+
+
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (role_id)
+  REFERENCES role(id)
+  ON DELETE restrict,
+  FOREIGN KEY (manager_id)
+  REFERENCES employee(id)
+  ON DELETE restrict
+);
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO department (id, name)
+VALUES (1, "Sales"),
+       (2, "Engineering"),
+       (3, "Finance"),
+       (4, "Legal");
+
+
